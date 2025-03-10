7d0dc6fe0cbba7f4ce4b9c63ccb6939ebd16d580
Ali Usman <aliusmanijaz143@gmail.com>
2025-03-10 15:01:47 +0500

    funtion of initializa and multiplhy materix

--- sequential.c
+++ sequential.c
@@ -17,3 +17,17 @@ void initializeMatrices()
         }
     }
 }
+
+void matrixMultiplication()
+{
+    for (int i = 0; i < SIZE; i++)
+    {
+        for (int j = 0; j < SIZE; j++)
+        {
+            for (int k = 0; k < SIZE; k++)
+            {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+}
