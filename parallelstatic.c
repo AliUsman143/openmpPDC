8ed32844b9b0855b9ec24d9d511eae4e498cb506
Ali Usman <aliusmanijaz143@gmail.com>
2025-03-10 16:31:00 +0500

    Add Function to Initialize Matrices

--- parallelstatic.c
+++ parallelstatic.c
@@ -6,3 +6,15 @@
 #define SIZE 500
 
 int A[SIZE][SIZE], B[SIZE][SIZE], C[SIZE][SIZE];
+void initializeMatrices()
+{
+    for (int i = 0; i < SIZE; i++)
+    {
+        for (int j = 0; j < SIZE; j++)
+        {
+            A[i][j] = rand() % 10;
+            B[i][j] = rand() % 10;
+            C[i][j] = 0;
+        }
+    }
+}
