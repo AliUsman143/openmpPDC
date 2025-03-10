f952fa4ff6ccd68182eb1f44cd0e51a69cdcacbf
Ali Usman <aliusmanijaz143@gmail.com>
2025-03-10 14:55:31 +0500

    Header Files and Global Declarations

--- sequential.c
+++ sequential.c
@@ -2,39 +2,18 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define SIZE 500  // Change size as needed
-
+#define SIZE 500
 int A[SIZE][SIZE], B[SIZE][SIZE], C[SIZE][SIZE];
 
-void initializeMatrices() {
-    for (int i = 0; i < SIZE; i++) {
-        for (int j = 0; j < SIZE; j++) {
+void initializeMatrices()
+{
+    for (int i = 0; i < SIZE; i++)
+    {
+        for (int j = 0; j < SIZE; j++)
+        {
             A[i][j] = rand() % 10;
             B[i][j] = rand() % 10;
             C[i][j] = 0;
         }
     }
-}
-
-void matrixMultiplication() {
-    for (int i = 0; i < SIZE; i++) {
-        for (int j = 0; j < SIZE; j++) {
-            for (int k = 0; k < SIZE; k++) {
-                C[i][j] += A[i][k] * B[k][j];
-            }
-        }
-    }
-}
-
-int main() {
-    srand(time(0));
-    initializeMatrices();
-        
-    clock_t start = clock();
-    matrixMultiplication();
-    clock_t end = clock();
-
-    printf("Execution Time: %f seconds\n", (double)(end - start) / CLOCKS_PER_SEC);
-    
-    return 0;
 }
